dc85ef6a783614764a36721d34839ff3019ec2c6
Anthony De Leon <tonton.dev001@gmail.com>
2017-07-17 04:38:09 +0800

    initial change for routing

--- core/routing.ts
+++ core/routing.ts
@@ -1,6 +1,7 @@
 import { checkAppConfig, Application, AppProvider } from './app';
 import { Request, HttpError, Response } from './http';
 import { FormValidationError } from './validation';
+import { Context } from './service';
 import * as express from 'express';
 
 /**
@@ -66,4 +67,83 @@ export namespace Router {
   export function create(options?: express.RouterOptions): express.Router {
     return express.Router(options);
   }
+}
+
+/**
+ * Abstract Controller class
+ */
+export abstract class Controller {
+
+  /**
+   * Controller constructor
+   */
+  constructor(protected context: Context) {}
+}
+
+/**
+ * Set flag for express to recognize exort controller
+ */
+(Controller as any).$exort = true;
+
+/**
+ * HttpControllerAsRouteOptions interface
+ */
+export interface HttpControllerAsRouterOptions {}
+
+/**
+ * Abstract HttpController class
+ */
+export abstract class HttpController extends Controller {
+
+  /**
+   * Express request object
+   */
+  protected readonly request: Request;
+
+  /**
+   * Express response object
+   */
+  protected readonly response: Response;
+
+  /**
+   *
+   */
+  public static asRouter(options?: HttpControllerAsRouterOptions): express.Router {
+    let router = express.Router();
+    return router;
+  }
+}
+
+/**
+ * RouteOptions interface
+ */
+export interface RouteOptions {}
+
+/**
+ * Route decorator for HttpController instance methods
+ */
+export function Route(method: string, options?: RouteOptions): (target: Object, key: string, desc: PropertyDescriptor) => void;
+
+/**
+ * Route decorator for HttpController instance methods
+ */
+export function Route(method: string, path?: string): (target: Object, key: string, desc: PropertyDescriptor) => void;
+
+/**
+ * Route decorator for HttpController instance methods
+ */
+export function Route(method: string, path?: string, options?: RouteOptions): (target: Object, key: string, desc: PropertyDescriptor) => void;
+
+/**
+ * Route decorator for HttpController instance methods
+ */
+export function Route(method: string, path?: string | RouteOptions, options?: RouteOptions) {
+  if (typeof path == 'object') {
+    options = path;
+    path = '/';
+  }
+
+  return (target: Object, propertyKey: string, desc: PropertyDescriptor) => {
+
+  };
 }
