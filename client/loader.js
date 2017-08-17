ace842487205b0d3299e92939fe6a7a83c396ae1
Anthony De Leon <tonton.dev001@gmail.com>
2017-08-18 05:05:04 +0800

    compiled

--- client/loader.js
+++ client/loader.js
@@ -10,12 +10,9 @@ var Loader = (function () {
         if (!className) {
             throw new Error("No extension name for " + this.resourcePath);
         }
-        var query = loaderUtils.getOptions(this) || {};
-        if (!query.name) {
-            query.name = this.resourcePath.replace(this.options.context.replace(/\/+$/, '') + "/", '');
-            query.name = query.name.substr(0, query.name.lastIndexOf('.')) || query.name;
-        }
-        var chunkNameParam = JSON.stringify(loaderUtils.interpolateName(this, query.name, { context: this.options.context }));
+        var name = this.resourcePath.replace(this.options.context.replace(/\/+$/, '') + "/", '');
+        name = name.substr(0, name.lastIndexOf('.')) || name;
+        var chunkNameParam = JSON.stringify(loaderUtils.interpolateName(this, name, { context: this.options.context }));
         return "module.exports = function(props) {\n  return require('exort/client').renderBundleComponent('" + className + "', props, function(cb) {\n    require.ensure([], function(require) {\n      cb(require(" + requireModuleName + "));\n    }, " + chunkNameParam + ");\n  });\n};";
     };
     return Loader;
