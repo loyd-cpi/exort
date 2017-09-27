ab7b2e2cfcebd9ddc50c0c8a7a8cb1889e6aa854
Anthony De Leon <tontondeleon03@gmail.com>
2017-09-27 11:22:13 +0800

    compiled

--- client/index.js
+++ client/index.js
@@ -5,8 +5,11 @@ require("reflect-metadata");
 tslib_1.__exportStar(require("./bundle"), exports);
 tslib_1.__exportStar(require("./component"), exports);
 tslib_1.__exportStar(require("./state"), exports);
+tslib_1.__exportStar(require("./util"), exports);
 var React = require("react");
 exports.React = React;
 var ReactDOM = require("react-dom");
 exports.ReactDOM = ReactDOM;
+var Immutable = require("immutable");
+exports.Immutable = Immutable;
 //# sourceMappingURL=index.js.map
